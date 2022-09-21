599efbf7cc748ee43c64a9dcc3b8412d82dc3e2c
Dylan Shaffer <SHAFFERDR@ETSU.EDU>
2022-09-21 10:41:08 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSF22CSCI3110Lab05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
